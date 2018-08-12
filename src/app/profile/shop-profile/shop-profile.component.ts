8ee42c928869584760cf3fa29577eb09412cce62
Unknown <victor_klemann@hotmail.com>
2018-08-12 16:27:19 -0300

    Incluído arquivo de configuração firebase

--- src/app/profile/shop-profile/shop-profile.component.ts
+++ src/app/profile/shop-profile/shop-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { NgForm } from '@angular/forms';
+
 import { Estabelecimento } from './shop-profile.model';
 
 @Component({
@@ -9,9 +12,16 @@ export class ShopProfileComponent implements OnInit {
 
   @Input() estabelecimento: Estabelecimento
 
-  constructor() { }
+  constructor(private angularFire: AngularFireDatabase) { }
 
   ngOnInit() {
   }
+
+  // form_submit(f: NgForm) {
+  //   this.angularFire.list("estabelecimentos")
+  //                   .push(this.estabelecimento)
+  //                   .then((t: any) => console.log('dados gravados: ' + t.key)), (e: any) => 
+  //                                     console.log(e.message);
+  // }
 
 }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+   apiKey: "AIzaSyB-I3vB8nuo8_Gyk8ESyLjHGllVF5GX4DQ",
+   authDomain: "beaty-time-d43a1.firebaseapp.com",
+   databaseURL: "https://beaty-time-d43a1.firebaseio.com",
+   projectId: "beaty-time-d43a1",
+   storageBucket: "simpleapprealtime-ef5c5.appspot.com",
+   messagingSenderId: "797595451568"
+};
